4748fd951ddfd27156d3407b356e74240164f6be
Kevin Chandler <im.kevin@me.com>
2019-01-09 11:00:51 -0800

    Lesson 21

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -51,6 +51,12 @@ class App extends React.Component {
     this.setState({fishes});
   };
 
+  deleteFish = key => {
+    const fishes = {...this.state.fishes};
+    fishes[key] = null;
+    this.setState({fishes});
+  };
+
   loadSampleFishes = () => {
     this.setState({fishes: sampleFishes});
   };
@@ -58,6 +64,12 @@ class App extends React.Component {
   addToOrder = key => {
     const order = {...this.state.order};
     order[key] = order[key] + 1 || 1;
+    this.setState({order});
+  };
+
+  removeFromOrder = key => {
+    const order = {...this.state.order};
+    delete order[key];
     this.setState({order});
   };
 
@@ -79,10 +91,15 @@ class App extends React.Component {
             })}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
+        />
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
         />
--- catch-of-the-day/src/components/EditFishForm.js
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -48,6 +48,12 @@ class EditFishForm extends React.Component {
           value={this.props.fish.image}
           onChange={this.handleChange}
         />
+        <button
+          onClick={() => {
+            this.props.deleteFish(this.props.index);
+          }}>
+          Remove Fish
+        </button>
       </div>
     );
   }
